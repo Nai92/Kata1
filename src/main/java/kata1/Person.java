446f7ee52274a12f3b41ad0f22deef8165711426
Nai92 <Nai92@users.noreply.github.com>
2020-10-07 14:17:22 +0100

    project created

---
+++ src/main/java/kata1/Person.java
@@ -0,0 +1,33 @@
+package kata1;
+
+import java.time.LocalDate;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthdate;
+    
+    public Person(final String name, LocalDate birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public LocalDate getBirthdate(){
+        return birthdate;
+    }
+    
+    public int getAge(){
+        return toYears(LocalDate.now().toEpochDay() - birthdate.toEpochDay());
+    }
+    
+    private int toYears(long days) {
+        return (int) (days/365);
+    }
+}
+
+
+
+
