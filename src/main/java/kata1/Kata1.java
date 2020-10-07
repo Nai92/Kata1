329567b4f9608ed92e422236d1df77e720176ccc
Nai92 <Nai92@users.noreply.github.com>
2020-10-07 14:11:15 +0100

    project created

---
+++ src/main/java/kata1/Kata1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata1;
+
+import java.time.LocalDate;
+import java.time.Month;
+/**
+ *
+ * @author Nigel
+ */
+public class Kata1 {
+    
+    
+    public static void main(String[] args) {
+        Person person = new Person("Luis Hernandez", LocalDate.of(1970, Month.MARCH, 30));
+        System.out.println(person.getName() + " tiene: " + person.getAge()+ " años");
+    }
+}
+
+
+
+
